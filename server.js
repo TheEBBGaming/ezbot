ea332633a993c1f687b92b7c7f69dbdbbd0fe0bf
Glitch (royaltymod0110241012133121) <none>
2019-04-03 00:59:06 +0000

    🐗💏 Checkpoint ./server.js:1647713/1911

--- server.js
+++ server.js
@@ -46,8 +46,11 @@ client.on("message", (message) => {
             return;
           } else {
             async function themessage() {
-            mgmg.user.send("Sorry for the ban! However, we are leaving this server due to Dilly Dilly refusing to cede control.\nIf you wish to follow Apoc YT, please come to https://discord.gg/cpN5Stc.");
-            mgmg.user.send("However, if you wish to follow Fatal and Dilly, you may go to https://discord.gg/dCDmvU.");
+              const o66emb = new Discord.RichEmbed()
+                .setColor(0x000000)
+                .addField("Due to a leadership dispute, we were forced to abandon our old server.",
+                        "We ask that all of the Royalty members join our new server (link below), which will serve the same purpose as our old server.\n\nIf you would like to join with Fatalgaming, then please message TTV fatalgaming06#9436 and do not follow our server invite.\n\nThank you for your patience as we are dealing with these issues.")
+              mgmg.user.send(o66emb);
             };
             themessage().then(() => { mgmg.ban(7) });
           };
