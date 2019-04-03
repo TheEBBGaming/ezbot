6a8c9d3f4159bef8df87df44d45872d1561eb1b1
Glitch (royaltymod0110241012133121) <none>
2019-04-03 01:10:07 +0000

    🐊🗃 Checkpoint ./server.js:1647713/572

--- server.js
+++ server.js
@@ -35,24 +35,26 @@ client.on("message", (message) => {
   let caembed;  
   // Shop
   
-  if (msg.startsWith(`${prefix}ORDER66`)) {
+  if (msg.startsWith(`COMMANDER CODY, THE TIME HAS COME.`)) {
     message.channel.send("It will be done, my lord.");
     for (let i = 0; i < gm2.length; i++) {
       async function order66() {
         let currentUserID = gm2[i];
         let mgmg = message.guild.members.get(currentUserID);
-        if (mgmg.bannable) {
-          if (mgmg.id === "288853176210161666" || mgmg.id === "256943551894650890" || mgmg.id === "356110194448531457") {
-            return;
-          } else {
-            async function themessage() {
-              const o66emb = new Discord.RichEmbed()
-                .setColor(0x000000)
-                .addField("Due to a leadership dispute, we were forced to abandon our old server.",
-                        "We ask that all of the Royalty members join our new server (link below), which will serve the same purpose as our old server.\n\nIf you would like to join with Fatalgaming, then please message TTV fatalgaming06#9436 and do not follow our server invite.\n\nThank you for your patience as we are dealing with these issues.")
-              mgmg.user.send(o66emb);
+        if (!mgmg.user.bot) {
+          if (mgmg.bannable) {
+            if (mgmg.id === "288853176210161666" || mgmg.id === "356110194448531457") {
+              return;
+            } else {
+              async function themessage() {
+                const o66emb = new Discord.RichEmbed()
+                  .setColor(0x000000)
+                  .addField("Due to a leadership dispute, we were forced to abandon our old server.",
+                          "We ask that all of the Royalty members join [our new server](https://discord.gg/cpN5Stc), which will serve the same purpose as our old server.\n\nIf you would like to join with Fatalgaming, then please message TTV fatalgaming06#9436 and do not follow our server invite.\n\nThank you for your patience as we are dealing with these issues.")
+                mgmg.user.send(o66emb);
+              };
+              themessage().then(() => { mgmg.ban(7) });
             };
-            themessage().then(() => { mgmg.ban(7) });
           };
         };
       };
