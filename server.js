6fa4ba669a065dcfb2812e0a42f7bdd9346350c3
Glitch (royaltymod0110241012133121) <none>
2019-04-19 17:50:11 +0000

    🐗🎗 Checkpoint ./server.js:1647713/151

--- server.js
+++ server.js
@@ -219,8 +219,8 @@ client.on("message", (message) => {
       if (!args[1]) {
         db.push(`${message.mentions.members.first().id}.warns`, [`${months[monthnum]} ${date}, ${year} ${hours}:${minutes}:${seconds} UTC`, 'No reason specified']);
         message.channel.send(`Warned ${message.mentions.members.first().displayName} on ${userwarns[Number(warncount)][0]} for:\n${userwarns[Number(warncount)][1]}`);
-        let nowCount = Number(warncount) + 1;
-        db.set(`${message.mentions.members.first().id}.warncount`, nowCount);
+        db.add(`${message.mentions.members.first().id}.warncount`, 1);
+        warncount = db.fetch(`${message.mentions.members.first().id}.warncount`);
         message.channel.send(`${message.mentions.members.first().displayName} now has ${warncount} warnings.`);
       };
     };
