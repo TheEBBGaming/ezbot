eb64f11ce3d1ee65430c93bfbce6ec7f0f0f0139
Glitch (royaltymod0110241012133121) <none>
2019-04-26 23:22:10 +0000

    🏔🎯 Checkpoint ./server.js:1647713/451

--- server.js
+++ server.js
@@ -487,7 +487,15 @@ client.on("message", (message) => {
         stringToSend = stringToSend + `\n[${i}] Warned on ${warnings[i][0]} - by ${warnings[i][2]}\nReason: ${warnings[i][1]}`;
       };
       let commandAuthor = message.author;
-      const collector = new Discord.MessageCollector(message.channel, m => commandAuthor.id == message.author.id, {  time: 30000, max: 30, maxMatches: 30 });
+      const collector = new Discord.MessageCollector(m => commandAuthor.id == message.author.id, {  time: 30000, max: 30, maxMatches: 30 });
+      collector.on('collect', message => {
+        if (message.content.toUpperCase() === 'all') {
+          if (userModRole === 'Moderator') return message.channel.send('Insufficient Permissions.');
+          for (let i = 0; i < warnings.length; i++) {
+            
+          };
+        };
+      });
     };
   };
   
