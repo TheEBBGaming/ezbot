c3e813cd3bcc4afc2b0bbd54ba4731b9455c554d
Glitch (royaltymod0110241012133121) <none>
2019-04-28 22:41:28 +0000

    🌖💡 Checkpoint ./server.js:1647713/1779

--- server.js
+++ server.js
@@ -504,14 +504,33 @@ client.on("message", (message) => {
         if (collected.first().content.toUpperCase() === "cancel") {
           return message.channel.send("Cancelled the command!"); 
         };
-        if (collected.content.toUpperCase() === "all" ) {
+        if (collected.first().content.toUpperCase() === "all" ) {
           db.delete(`${toClear.id}.warns`);
           let clearedEmbed = new Discord.RichEmbed()
             .setAuthor(`Cleared all warnings for ${toClear.displayName}${mmmfTag} at ${months[monthnum]} ${date}, ${year} ${hours}:${minutes}:${seconds} UTC`, toClear.user.avatarURL)
-            .addField(`Cleared By:`, `${message.guild.members.get(commandAuthor.id).displayName}${authorTag}`)
+            .addField(`Cleared By:`, `${message.guild.members.get(collected.first().author.id).displayName}${authorTag}`)
           logchannel.send(clearedEmbed);
           message.channel.send(clearedEmbed);
+          return;
+        } else if (!isNaN(collected.first().content.toUpperCase)) {
+          if (warnDB[Number(collected.first().content)] === undefined) return;
+          warnDB.splice(Number(collected.first().content), 1);
+          db.set(`${toClear.warns}`, warnDB);
+          warnDB = db.fetch(`${toClear.id}.warns`);
+          let clearedEmbed = new Discord.RichEmbed()
+            .setAuthor(`Cleared warning number ${collected.first().content} for ${toClear.displayName}${mmmfTag} at ${months[monthnum]} ${date}, ${year} ${hours}:${minutes}:${seconds} UTC`, toClear.user.avatarURL)
+            .addField("Warning Info:", `Warned at ${warnDB[Number(collected.first().content)][0]} by ${warnDB[Number(collected.first().content)][2]}\n\n**Reason**: ${warnDB[Number(collected.first().content)][1]}`)
+            .addField("Cleared By:", `${message.guild.members.get(collected.first().author.id).displayName}${authorTag}`)
+          logchannel.send(clearedEmbed);
+          message.channel.send(clearedEmbed);
+          return;
+        } else {
+          return message.channel.send('Error. Invalid input. Please run the command again.');
         };
+      })
+      .catch(e => {
+        console.log(e);
+        message.channel.send(
       })
       const collector = new Discord.MessageCollector(m => commandAuthor.id === m.author.id, {  time: 30000, max: 30 });
       collector.on('collect', message => {
