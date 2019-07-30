8d77c38bf872a3dbf0f48dec27c3c178f2de8d37
Glitch (royaltymod0110241012133121) <none>
2019-07-30 01:39:41 +0000

    🐆💋 Checkpoint ./server.js:1647713/3306

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const BrawlStars = require('brawlstars');
 const fs = require('fs');
 const vision = require('@google-cloud/vision');
 const visionClient = new vision.ImageAnnotatorClient();
+const XMLHttpRequest = require('xhr2');
 const bsClient = new BrawlStars.Client({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkaXNjb3JkX3VzZXJfaWQiOiIyODg4NTMxNzYyMTAxNjE2NjYiLCJpYXQiOjE1NTE0OTAzMTV9.ahSIX-b6ZjWPI2EdtyoGXAK-brDW9fx6vpociyCW8jw" });
 const http = require('http'); const express = require('express'); const app = express(); app.get("/", (request, response) => { response.sendStatus(200); }); app.listen(process.env.PORT); setInterval(() => { http.get(`http://royaltymod312112133.glitch.me/`); }, 80000)
 
